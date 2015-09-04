e2307d65d68b44cdf3cb8b2cfd7b1e3fa131a2dd
ohhheyyyy <ashley.hartt@gmail.com>
2015-09-04 19:55:33 -0400

    Remove <br/> in alert; delete superfluous code

--- projects/tile_match_maker/memory.js
+++ projects/tile_match_maker/memory.js
@@ -71,7 +71,7 @@ function flipDatTile(tile,val){
             	tileIds = [];
 				// Check to see if the whole board is cleared
 				if(tilesFlipped == tileArray.length){
-					alert("BOOM! BOARD CLEARED. YOU WON!<br />Let's play again.");
+					alert("BOOM! BOARD CLEARED. YOU WON! Let's play again.");
 					document.getElementById('memoryGameBoard').innerHTML = "";
 					brandNewBoard();
 				}
@@ -92,74 +92,4 @@ function flipDatTile(tile,val){
 			}
 		}
 	}
-
-
-// var tileArray = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H', 'I', 'I', 'J', 'J', 'K', 'K', 'L', 'L'];
-// var tileValues = [];
-// var tileIds = [];
-// var tilesFlipped = 0;
-
-
-// // Board shuffle array function
-// Array.prototype.tileShuffle = function() {
-//     var i = this.length,
-//         j, temp;
-//     while (--i > 0) {
-//         j = Math.floor(Math.random() * (i + 1));
-//         temp = this[j];
-//         this[j] = this[i];
-//         this[i] = temp;
-//     }
-// };
-
-// // Generate new board function 
-// function newBoard() {
-//     tilesFlipped = 0;
-//     var output = '';
-//     tileArray.tileShuffle();
-//     for (var i = 0; i < tileArray.length; i++) {
-//         output += '<div id="tile_' + i + '" onclick="flipDatTile(this,\'' + tileArray[i] + '\')"></div>';
-//     }
-//     document.getElementById("memoryGameBoard").innerHTML = output;
-// }
-
-// // Flip tile function
-// function flipDatTile(tile, val) {
-//     if (tile.innerHTML === "" && tileValues < 2) {
-//         tile.style.background = '#F62459';
-//         tile.innerHTML = val;
-//         if (tileValues.length === 0) {
-//             tileValues.push(val);
-//             tileIds.push(tile.id);
-//         } else if (tileValues.length == 1) {
-//             tileValues.push(val);
-//             tileIds.push(tile.id);
-//             if (tileValues[0] == tileValues[1]) {
-//                 tilesFlipped += 2;
-//                 // clear the arrays
-//                 tileValues = [];
-//                 tileIds = [];
-//                 // is the whole board cleared? let's check
-//                 if (tilesFlipped === tileArray.length) {
-//                     alert("BOOM! BOARD CLEARED. YOU WON!<br />Let's play again.");
-//                     document.getElementById("memoryGameBoard").innerHTML = "";
-//                     newBoard();
-//                 } else {
-//                     function flip2Back() {
-//                         // Flip the 2 tiles back over
-//                         var tile_1 = document.getElementById(tileIds[0]);
-//                         var tile_2 = document.getElementById(tileIds[1]);
-//                         tile_1.style.background = 'url(tiles.jpg) no-repeat';
-//                         tile_1.innerHTML = "";
-//                         tile_2.style.background = 'url(tiles.jpg) no-repeat';
-//                         tile_2.innerHTML = "";
-//                         // clear the arrays
-//                         tileValues = [];
-//                         tileIds = [];
-//                     }
-//                     setTimeout(flip2Back, 700);
-//                 }
-//             }
-//         }
-//     }
 }
